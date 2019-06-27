1778c1ec77ec79dff6d74ca7306a74d563ec92c0
James Declerus <james.declerus@esih.edu>
2019-06-27 15:59:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
-
+require_relative 'models/piglatinizer.rb'
 class App < Sinatra::Base
+  get '/' do
+  erb :user_input.erb
+end
+
+post '/piglatinize' do
+  p = PigLatinize.new
+  @piglatin = p.to_pig_latin(params[:user_phrase])
+  erb :piglatinize
+end
 end
